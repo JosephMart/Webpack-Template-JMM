613ff501917608115ef6a338510e1e45d328cefd
Joseph Martinsen <joseph@martinsen.com>
2018-04-04 02:04:18 -0500

    Finished basic code splitting with Lazy loading

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,19 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: path.resolve(__dirname, '../src/index.js'),
+        index: path.resolve(__dirname, '../src/index.js'),
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        }),
+        })
     ],
     output: {
         filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, '../dist'),
         publicPath: '/'
     },
