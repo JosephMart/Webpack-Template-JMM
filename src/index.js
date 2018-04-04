62ce6cef058791ce635123b8b27f68e109c31613
Joseph Martinsen <joseph@martinsen.com>
2018-04-04 01:10:18 -0500

    Finished Output Management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    // Add image
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-    console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
     return element;
 }
 document.body.appendChild(component());
--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'),
+    entry: {
+        app: path.resolve(__dirname, '../src/index.js'),
+        print: path.resolve(__dirname, '../src/print.js')
+    },
+    devtool: 'inline-source-map', // not for prod
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, '../dist')
     },
     module: {
