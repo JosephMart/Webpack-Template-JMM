8c95e207a1954a85a82e030f199c63e22a7089e0
Joseph Martinsen <joseph@martinsen.com>
2018-04-04 00:54:41 -0500

    Finished Asset Management

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -5,5 +5,45 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            // This enables you to import './style.css' into the file that depends on that styling.
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // For images
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            // For fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            // For CSVs and TSVs
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            // For XML
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 };
